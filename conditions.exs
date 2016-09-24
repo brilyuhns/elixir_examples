f1e6f28c200a966632515a51fb8beef1ce29a426
Divya Kunnath <kunnathdiyval@gmail.com>
2016-09-24 16:47:42 +0530

    conditions and module

---
+++ conditions.exs
@@ -0,0 +1,15 @@
+defmodule Conditions do
+  def case_test1(list) do 
+    case list do
+      {4, 5, 6} ->
+        "This clause won't match"
+      {1, x, 3} ->
+        IO.puts x
+        "This clause will match and bind x to 2 in this clause"
+      _ ->
+        "This clause would match any value"
+    end
+  end
+end
+
+IO.puts Conditions.case_test1({1, 2, 3})
--- simple.exs
+++ simple.exs
@@ -1,11 +1,14 @@
 IO.puts "Hello world from Elixir"
 
-
-case {1, 2, 3} do
-  {4, 5, 6} ->
-    "This clause won't match"
-  {1, x, 3} ->
-    "This clause will match and bind x to 2 in this clause"
-  _ ->
-    "This clause would match any value"
+def case_test1(list) do 
+  case list do
+    {4, 5, 6} ->
+      "This clause won't match"
+    {1, x, 3} ->
+      "This clause will match and bind x to 2 in this clause"
+    _ ->
+      "This clause would match any value"
+  end
 end
+
+case_test1({1, 2, 3})
