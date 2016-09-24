cca59c9457573f8a0331883e1af6a444f89b3b56
Divya Kunnath <kunnathdiyval@gmail.com>
2016-09-24 16:54:45 +0530

    match against existing variable and string interpolation

--- conditions.exs
+++ conditions.exs
@@ -4,12 +4,29 @@ defmodule Conditions do
       {4, 5, 6} ->
         "This clause won't match"
       {1, x, 3} ->
-        IO.puts x
+        IO.puts "x = #{x}"
         "This clause will match and bind x to 2 in this clause"
       _ ->
         "This clause would match any value"
     end
   end
+
+  def match_against_existing_variable(x) do
+    case 10 do 
+      ^x ->
+        "case 10 is matched"
+      _ ->
+        "Else case matched"
+    end
+  end
 end
 
+IO.puts "Conditions.case_test1({1, 2, 3})"
 IO.puts Conditions.case_test1({1, 2, 3})
+IO.puts "--------"
+IO.puts "Conditions.match_against_existing_variable(10)"
+IO.puts Conditions.match_against_existing_variable(10)
+IO.puts "--------"
+IO.puts "Conditions.match_against_existing_variable(2)"
+IO.puts Conditions.match_against_existing_variable(2)
+IO.puts "--------"
