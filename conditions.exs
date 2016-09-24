af31b255f517c0bfa52d36393dd2d4120cb2e6c1
Divya Kunnath <kunnathdiyval@gmail.com>
2016-09-24 17:02:13 +0530

    case with and without guard conditions and ordering

--- conditions.exs
+++ conditions.exs
@@ -19,6 +19,17 @@ defmodule Conditions do
         "Else case matched"
     end
   end
+
+  def clause_with_guards(list) do 
+    case list do
+      {1, x, 3} when x > 0 ->
+        "List with gaurd condition matched"      
+      {1, x, 3} ->
+        "List without gaurd condition matched"
+      _ ->
+        "list with or without guard condition not matched"
+    end
+  end
 end
 
 IO.puts "Conditions.case_test1({1, 2, 3})"
@@ -29,4 +40,13 @@ IO.puts Conditions.match_against_existing_variable(10)
 IO.puts "--------"
 IO.puts "Conditions.match_against_existing_variable(2)"
 IO.puts Conditions.match_against_existing_variable(2)
+IO.puts "--------"
+IO.puts "Conditions.clause_with_guards({1, 2, 3})"
+IO.puts Conditions.clause_with_guards({1, 2, 3})
+IO.puts "--------"
+IO.puts "Conditions.clause_with_guards({1, -1, 3})"
+IO.puts Conditions.clause_with_guards({1, -1, 3})
+IO.puts "--------"
+IO.puts "Conditions.clause_with_guards({4, 5, 6})"
+IO.puts Conditions.clause_with_guards({4, 5, 6})
 IO.puts "--------"
