9025dc09c60b86368c541f4e64fc5102ab1b3fbc
Divya Kunnath <kunnathdiyval@gmail.com>
2016-09-24 15:51:32 +0530

    simple_examples

---
+++ simple.exs
@@ -0,0 +1,11 @@
+IO.puts "Hello world from Elixir"
+
+
+case {1, 2, 3} do
+  {4, 5, 6} ->
+    "This clause won't match"
+  {1, x, 3} ->
+    "This clause will match and bind x to 2 in this clause"
+  _ ->
+    "This clause would match any value"
+end
